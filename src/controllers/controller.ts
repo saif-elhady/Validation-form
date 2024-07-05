0edd1b41a95523c33379071cf62f20995f6889c2
saif-elhady <saifelhady2004@gmail.com>
2024-07-05 21:21:08 +0300

    test

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,10 +1,37 @@
 import { Request, Response } from "express"; 
+import { check, validationResult } from "express-validator";
+
 
 //GET HOMEPAGE
 const formIndex = (req: Request, res: Response) => {
     res.render('index');
 }
 
+//POST REGISTER
+const postRegister = [ check('fullname', 'fullname is required')
+    .exists(),
+check('email', 'email is not valid')
+    .isEmail()
+    .custom((value) => value.endsWith('@gmail.com')),
+check('password', 'password must be at least 5 characters long')
+    .isLength({ min: 5 }),
+check('password-confirm', 'password doesn\'t match')
+    .custom((value, { req }) => value === req.body.password),
+check('birthdate', 'Enter a valid birthday in MM/DD/YYYY format')
+        .isDate({ format: 'MM-DD-YYYY' }),
+    (req: Request, res: Response) => {
+    const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+        return res.status(422).jsonp(errors.array())
+        // // const alert = errors.array()
+        // // res.render('index', {
+        // //     alert
+        // })
+    }
+    return;
+}];
 export default {
-    formIndex
+    formIndex,
+    postRegister
 }
+
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import controller from '../controllers/controller';
+import { urlencoded } from "body-parser";
+
 
 const router = express.Router();
 
 router.get('/', controller.formIndex);
-
+router.post('/', urlencoded, controller.postRegister);
 export default router;
